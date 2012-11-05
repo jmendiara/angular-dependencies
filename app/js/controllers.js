940088d09d9c7154b35fa76ed8b78ce734d85753
Javier Mendiara Caardo <javier@tid.es>
2012-11-05 09:00:26 +0100

    IMPLEMENT first quick proof of concept

---
+++ app/js/controllers.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/* Controllers */
+
+
+var providers = {};
+angular.module('ng')
+    .config(['$provide', function ($provide){
+      providers = $provide.getProviders();
+    }]);
+
+
+function CoreController($scope) {
+
+  $scope.nodes = [];
+  angular.forEach(providers, function (provider, providerName){
+    providerName = providerName.replace('Provider', '');
+    if (providerName === '$injector'  || providerName === '$provide'  ) return;
+      $scope.nodes.push(providerName);
+  });
+
+  var injector = angular.injector(['ng']);
+
+  $scope.edges = [];
+  angular.forEach(providers, function (provider, i){
+    if (!provider.$get) return;
+    var injected = injector.annotate(provider.$get);
+    $scope.edges = $scope.edges.concat(createEdges(i, injected));
+
+  });
+
+
+  function createEdges(provider, dest){
+    var ret = [],
+        provider = provider.replace('Provider', '');
+
+    angular.forEach(dest, function (edge){
+      if (edge === '$injector'  || edge === '$provide'  ) return;
+      if (!providers[edge+"Provider"]){
+        console.log("aaaaaaaaaaaaaato", edge);
+        return;
+      }
+      ret.push ({src: provider, dst: edge});
+    });
+
+    return ret;
+  }
+
+
+
+
+
+}
+CoreController.$inject = ['$scope'];
+
---
+++ app/js/directives.js
@@ -0,0 +1,135 @@
+'use strict';
+
+/* Directives */
+
+
+sigma.publicPrototype.myCircularLayout = function() {
+  var R = 0.4,
+      i = 0,
+      L = this.getNodesCount();
+
+  this.iterNodes(function(n){
+    n.x = (Math.cos(Math.PI*(i++)/L)*R);
+    n.y = (Math.sin(Math.PI*(i++)/L)*R);
+  });
+
+  return this.position(0,0,1).draw();
+};
+
+angular.module('ngDeps.directives', []).
+    directive('appName', ['name', function ($name){
+      return function(scope, elm, attrs) {
+        elm.text($name);
+      };
+    }]).
+    directive('angularVersion', function() {
+      return function(scope, elm, attrs) {
+        elm.text(angular.version.full);
+      };
+    }).
+    directive('sigmaGraph', function() {
+      return function(scope, domElement, attrs) {
+        var sigInst = sigma.init(domElement[0]);
+
+
+        var greyColor = '#666';
+        sigInst.bind('overnodes',function(event){
+          var nodes = event.content;
+          var neighbors = {};
+          sigInst.iterEdges(function(e){
+            if(nodes.indexOf(e.source)<0 && nodes.indexOf(e.target)<0){
+              if(!e.attr['grey']){
+                e.attr['true_color'] = e.color;
+                e.color = greyColor;
+                e.attr['grey'] = 1;
+              }
+            }else{
+              e.color = e.attr['grey'] ? e.attr['true_color'] : e.color;
+              e.attr['grey'] = 0;
+
+              neighbors[e.source] = 1;
+              neighbors[e.target] = 1;
+            }
+          }).iterNodes(function(n){
+                if(!neighbors[n.id]){
+                  if(!n.attr['grey']){
+                    n.attr['true_color'] = n.color;
+                    n.color = greyColor;
+                    n.attr['grey'] = 1;
+                  }
+                }else{
+                  n.color = n.attr['grey'] ? n.attr['true_color'] : n.color;
+                  n.attr['grey'] = 0;
+                }
+              }).draw(2,2,2);
+        }).bind('outnodes',function(){
+              sigInst.iterEdges(function(e){
+                e.color = e.attr['grey'] ? e.attr['true_color'] : e.color;
+                e.attr['grey'] = 0;
+              }).iterNodes(function(n){
+                    n.color = n.attr['grey'] ? n.attr['true_color'] : n.color;
+                    n.attr['grey'] = 0;
+                  }).draw(2,2,2);
+            });
+
+        sigInst.bind('overnodes',function(event){
+          var nodes = event.content;
+          var target = event.content[0];
+          var neighbors = {};
+          sigInst.iterEdges(function(e){
+            if(nodes.indexOf(e.source)>=0 || nodes.indexOf(e.target)>=0){
+              neighbors[e.source] = 1;
+              neighbors[e.target] = 1;
+            }
+          }).iterNodes(function(n){
+            if(!neighbors[n.id]){
+              n.hidden = 1;
+            }else {
+              n.hidden = 0;
+            }
+            if (n.id === target){
+              n.hidden = 0;
+            }
+          });
+          event.target    .get
+          sigInst.draw(2,2,2);
+        }).bind('outnodes',function(){
+          sigInst.iterEdges(function(e){
+            e.hidden = 0;
+          }).iterNodes(function(n){
+            n.hidden = 0;
+          }).draw(2,2,2);
+        });
+
+
+        sigInst.drawingProperties({
+          defaultLabelColor: '#ccc',
+          font: 'Arial',
+          edgeColor: 'source',
+          defaultEdgeType: 'curve'
+        }).graphProperties({
+              minNodeSize: 1,
+              maxNodeSize: 10
+        });
+
+        //console.log(scope);
+        angular.forEach(scope.nodes, function (node){
+          sigInst.addNode(node,{
+            label: node,
+            color: 'rgb('+Math.round(Math.random()*256)+','+
+                Math.round(Math.random()*256)+','+
+                Math.round(Math.random()*256)+')'
+          });
+        });
+
+        var id = 0;
+        angular.forEach(scope.edges, function (edge){
+          sigInst.addEdge(id++, edge.src, edge.dst);
+        });
+
+        sigInst.draw();
+        sigInst.myCircularLayout();
+        //console.log(scope);
+      };
+    })
+;
